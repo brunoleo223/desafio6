4a9fbdb6f1400259ff4f29c48ab34c107aa753f4
Bruno Leonardo <brunoleo223@gmail.com>
2020-09-19 18:48:18 -0300

    Entrega do Desafio 6

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getCustomRepository} from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,8 +11,29 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
-  }
-}
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transactions = await transactionRepository.find();
+    const { income, outcome } = transactions.reduce(
+      (acumulator: Balance, transaction: Transaction) => {
+        if (transaction.type == 'income') {
+          acumulator.income += transaction.value;
+          acumulator.total += transaction.value;
+        } else if (transaction.type == 'outcome') {
+          acumulator.outcome += transaction.value;
+          acumulator.total -= transaction.value;
+        }
+        return acumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0
+      }
+      )
 
-export default TransactionsRepository;
+      const total = income - outcome;
+      return { income, outcome, total };
+    }
+  }
+
+  export default TransactionsRepository;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,24 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+interface Request{
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if(!transaction){
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(transaction);
   }
+
 }
 
 export default DeleteTransactionService;
